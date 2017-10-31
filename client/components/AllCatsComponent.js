da84636cc7125feaf76f304d415d09df8dd88e21
sreepriya <sreepriyav@live.com>
2017-10-31 18:13:13 -0400

    inprogress

--- client/components/AllCatsComponent.js
+++ client/components/AllCatsComponent.js
@@ -1,32 +1,76 @@
-import React from 'react'
+import React from "react";
 
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 
 /**
  * COMPONENT
  */
-export const AllCatsComponent = (props) => {
-  const {cats} = props
-
-  return (
-    <div>
-
-    {
-        cats.map(cat=><li key={cat.id}>{cat.name}</li>)
-      
+class AllCatsComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: "",
+      cats:[]
     }
-    </div>
-  )
+    this.onSelectSubmit = this.onSelectSubmit.bind(this);
+  }
+
+  onSelectSubmit(event) {
+    this.setState({ category: event.target.value });
+  }
+
+  onSelectCategory()
+  {
+    
+  }
+  render() {
+    const { cats } = props;
+
+    return (
+      <div>
+        <ul>{cats.map(cat => <li key={cat.id}>{cat.name}</li>)}</ul>
+
+        <div>
+          <select  onChange={this.onSelectSubmit}>
+            {/* <option>AGE</option>
+            <option value="kitten"> Kitten</option> //if age less than 10
+            <option value="adult"> Adult</option> //if age greater than 10 */}
+            {/* <option>Color</option>
+
+            {cats.map(cat => (
+              <option key={cat.id} value={cat.color}>
+                {" "}
+                cat.color
+              </option>
+            ))} */}
+            <option>HairLength</option>
+            
+            
+            <option>Profession</option>
+           
+            ))}
+            {/* <option>Breed</option>
+            {cats.map(cat => (
+              <option key={cat.id} value={cat.breed}>
+                {" "}
+                cat.breed
+              </option>
+            ))} */}
+          </select>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     cats: state.cats
-  }
-}
+  };
+};
 
-export default connect(mapState)(AllCatsComponent)
-
+export default connect(mapState)(AllCatsComponent);
