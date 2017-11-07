6daa388b3e587e1946abc5fb18ef47f9afe146a6
sreepriya <sreepriyav@live.com>
2017-11-07 15:14:19 -0500

    testing

--- server/test/ordersRoutes.spec.js
+++ server/test/ordersRoutes.spec.js
@@ -1,57 +1,37 @@
-
 const expect = require('chai').expect;
-require('chai').use(require('chai-as-promised'))
-const agent = require('supertest')(require('../'))
+require('chai').use(require('chai-as-promised'));
+const agent = require('supertest')(require('../'));
 const { Orders } = require('../db/models');
 const { db } = require('../db/db.js');
-const Promise = require('bluebird');
-// Does this:
-//
-// const createAgent = require('supertest')
-// const app = require('../app')
-// const agent = createAgent(app)
 
-describe('http requests', function () {
-  before(() => Orders.sync({force: true}))
+describe('http requests', function() {
+  before(() => Orders.sync({ force: true }));
 
-  describe('GET /api/orders/', function () {
-    it('responds with 200', () =>
-      agent.get('/api/orders/')
-        .expect(200))
+  describe('GET /api/orders/', function() {
+    it('responds with 200', () => agent.get('/api/orders/').expect(200));
   });
 
-  
-
-  describe('GET /api/orders/:userId', function () {
+  describe('GET /api/orders/:userId', function() {
     before(() =>
       Orders.create({
         status: 'Created',
-        totalPrice: 400.00
-      
+        totalPrice: 400.0
       })
-    )
+    );
 
-    it('responds with 404 on page that does not exist',
-      () =>
-        agent.get('/api/orders/:500')
-          .expect(404))
-
+    it('responds with 404 on page that does not exist', () =>
+      agent.get('/api/orders/:500').expect(404));
   });
 
-
-  describe('GET /api/orders/:userId/:orderId', function () {
+  describe('GET /api/orders/:userId/:orderId', function() {
     before(() =>
       Orders.create({
         status: 'Created',
-        totalPrice: 400.00
+        totalPrice: 400.0
       })
-    )
+    );
 
-    it('responds with 404 on page that does not exist',
-      () =>
-        agent.get('/api/orders/:100/:200')
-          .expect(404))
-
+    it('responds with 404 on page that does not exist', () =>
+      agent.get('/api/orders/:100/:200').expect(404));
   });
-
 });
